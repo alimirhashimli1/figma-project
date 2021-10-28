00e28846fc3c5ad1a00c42c8e1a3e258ec49c3e7
alimirhashimli <alimirhashimli@gmail.com>
2021-10-28 10:31:01 +0200

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # figma-project
+
+## Preview of the page on-off toggle
+
+![](screenshot-1.png)
+![](screenshot-2.png)
---
+++ app.js
@@ -0,0 +1,13 @@
+const turnOff = document.querySelector('.turn-off')
+const turnOff1 = document.querySelector('.turn-off-1')
+const nav = document.querySelector('nav')
+
+
+function myFunction() {
+    // turnOff.classList.toggle('background-switch')
+    // turnOff1.classList.toggle('background-switch')
+    nav.classList.toggle('nav-color')
+    document.body.classList.toggle('body-color')
+    
+
+}
